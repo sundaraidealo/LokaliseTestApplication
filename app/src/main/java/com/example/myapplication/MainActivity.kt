85552b0b4b324c3c8ebe7ae0641024273528de8c
Sundara Raghavan Santhanam <sundara.santhanam@idealo.de>
2021-07-09 21:24:17 +0200

    Add localise support and add a new dynamic string

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -9,12 +10,18 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import co.lokalise.android.sdk.core.LokaliseContextWrapper
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
 	private lateinit var appBarConfiguration: AppBarConfiguration
 	private lateinit var binding: ActivityMainBinding
+
+	override fun attachBaseContext(newBase: Context) {
+		// Inject the Lokalise SDK into the activity context
+		super.attachBaseContext(LokaliseContextWrapper.wrap(newBase))
+	}
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/myapplication/MyApplication.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication
+
+import android.app.Application
+import co.lokalise.android.sdk.LokaliseSDK
+
+/**
+ * @author sundara.santhanam
+ * @since 09.07.2021
+ */
+
+class MyApplication :Application(){
+	override fun onCreate() {
+		super.onCreate()
+		// Initialise Lokalise SDK with projects SDK token and project id
+		// It is important to call this right after the "super.onCreate()"
+		// If you are using AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+		// make sure it is called before LokaliseSDK.init()
+		LokaliseSDK.init( "9f45cea43afd721d751ca0fad8689fb59efbb3d4", "5588096260e6beffb43ad9.55374331",this);
+
+		// Add this only if you want to use pre-release bundles
+		LokaliseSDK.setPreRelease(true);
+
+		// Fetch the latest translations from Lokalise (can be called anywhere)
+		LokaliseSDK.updateTranslations();
+	}
+}
